39157c4ec1fdb59f259cb515db6407f82332ac31
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2023-01-27 16:30:58 -0600

    m

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 
 const Register = () => {
-    const[username, setUsername] = useState('');
+    const[name, setName] = useState('');
     const[email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [atype, setAtype] = useState('');
@@ -44,7 +44,7 @@ const Register = () => {
                         <form>
                             <div className="mb-3">
                                 <label htmlFor="username" className="form-label">Username</label>
-                                <input type="test" className="form-control" id="username" onChange={(e)=>setUsername(e.target.value)} />
+                                <input type="test" className="form-control" id="name" onChange={(e)=>setName(e.target.value)} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="email" className="form-label">Email</label>
